4bb8b4ff0ea8e80baf579d19954a8ebfa5ca591e
Tharun <168743888+Tharun-AddOnn@users.noreply.github.com>
2024-07-15 14:33:17 +0530

    adding @available(iOS 16.0, *)

--- ios/Sources/LidarPlugin/MyViewController.swift
+++ ios/Sources/LidarPlugin/MyViewController.swift
@@ -1,6 +1,8 @@
 import UIKit
 import Capacitor
 
+@available(iOS 16.0, *)
+
 class MyViewController: CAPBridgeViewController {
     override open func capacitorDidLoad() {
         bridge?.registerPluginInstance(LidarPlugin())
